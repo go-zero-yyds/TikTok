fb882f871258cbdd74dec37941267012fd1735aa
hackerxiao <hackerxiao@foxmail.com>
2023-07-31 21:44:15 +0800

    feat: 初始化数据库连接

--- apps/video/rpc/internal/svc/servicecontext.go
+++ apps/video/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "rpc/apps/video/rpc/internal/config"
+import (
+	"rpc/apps/video/model"
+	"rpc/apps/video/rpc/internal/config"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	Model  model.VideoModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		Model:  model.NewVideoModel(sqlx.NewMysql(c.DataSourse), c.Cache),
 	}
 }
