7320d5ed9302e0c318489bb75c270d8841007939
r27153733 <r27153733@gmail.com>
2023-08-03 11:40:39 +0800

    feat: 完善api定义。

--- apps/app/api/internal/config/config.go
+++ apps/app/api/internal/config/config.go
@@ -1,7 +1,20 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	rest.RestConf
+	// RPC 配置
+	VideoRPC       zrpc.RpcClientConf
+	UserRPC        zrpc.RpcClientConf
+	InteractionRPC zrpc.RpcClientConf
+	SocialRPC      zrpc.RpcClientConf
+	// JWT 配置
+	JwtAuth struct {
+		AccessSecret string
+		AccessExpire int64
+	}
 }
--- apps/app/api/internal/svc/servicecontext.go
+++ apps/app/api/internal/svc/servicecontext.go
@@ -2,14 +2,33 @@ package svc
 
 import (
 	"TikTok/apps/app/api/internal/config"
+	"TikTok/apps/app/api/utils/auth"
+	"TikTok/apps/interaction/rpc/interactionclient"
+	"TikTok/apps/social/rpc/socialclient"
+	"TikTok/apps/user/rpc/userclient"
+	"TikTok/apps/video/rpc/videoclient"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config         config.Config
+	UserRPC        userclient.User
+	VideoRPC       videoclient.Video
+	InteractionRPC interactionclient.Interaction
+	SocialRPC      socialclient.Social
+	JwtAuth        auth.JwtAuth
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:         c,
+		UserRPC:        userclient.NewUser(zrpc.MustNewClient(c.UserRPC)),
+		VideoRPC:       videoclient.NewVideo(zrpc.MustNewClient(c.VideoRPC)),
+		InteractionRPC: interactionclient.NewInteraction(zrpc.MustNewClient(c.InteractionRPC)),
+		SocialRPC:      socialclient.NewSocial(zrpc.MustNewClient(c.SocialRPC)),
+		JwtAuth: auth.JwtAuth{
+			AccessSecret: []byte(c.JwtAuth.AccessSecret),
+			AccessExpire: c.JwtAuth.AccessExpire,
+		},
 	}
 }
