076fd653f6d15d5ef39e85b6863b695d5d2e112f
r27153733 <r27153733@gmail.com>
2023-08-21 16:55:41 +0800

    feat: 增加redis服务。

--- apps/interaction/rpc/internal/svc/servicecontext.go
+++ apps/interaction/rpc/internal/svc/servicecontext.go
@@ -3,6 +3,7 @@ package svc
 import (
 	"TikTok/apps/interaction/rpc/internal/config"
 	"TikTok/apps/interaction/rpc/model"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 
 	"github.com/bwmarrin/snowflake"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
@@ -12,6 +13,7 @@ type ServiceContext struct {
 	Config    config.Config
 	Snowflake *snowflake.Node
 	DBAction  *model.DBAction
+	Rds       *redis.Redis
 }
 
 func NewServiceContext(c config.Config) (*ServiceContext, error) {
@@ -20,9 +22,11 @@ func NewServiceContext(c config.Config) (*ServiceContext, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return &ServiceContext{
 		Config:    c,
 		Snowflake: node,
 		DBAction:  model.NewDBAction(sqlx.NewMysql(c.DBSource), c.Cache),
+		Rds:       redis.MustNewRedis(c.Redis.RedisConf),
 	}, nil
 }
