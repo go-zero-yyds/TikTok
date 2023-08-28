ed9c87b776581ee04711b713323ea73ad82032c6
lucky-zzz <1418170131@qq.com>
2023-08-28 12:47:02 +0800

    feat:配置

--- apps/video/rpc/internal/svc/servicecontext.go
+++ apps/video/rpc/internal/svc/servicecontext.go
@@ -3,14 +3,19 @@ package svc
 import (
 	"TikTok/apps/video/rpc/internal/config"
 	"TikTok/apps/video/rpc/model"
+
 	"github.com/bwmarrin/snowflake"
+	"github.com/redis/go-redis/v9"
+	"github.com/zeromicro/go-queue/kq"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config    config.Config
-	Model     model.VideoModel
-	Snowflake *snowflake.Node
+	Config      config.Config
+	Model       model.VideoModel
+	Snowflake   *snowflake.Node
+	RedisClient *redis.Client
+	KafkaPusher *kq.Pusher
 }
 
 func NewServiceContext(c config.Config) (*ServiceContext, error) {
@@ -24,5 +29,10 @@ func NewServiceContext(c config.Config) (*ServiceContext, error) {
 		Config:    c,
 		Model:     model.NewVideoModel(sqlx.NewMysql(c.DBSource), c.Cache),
 		Snowflake: node,
+		RedisClient: redis.NewClient(&redis.Options{
+			Addr:     c.Redisconf.Host,
+			Password: c.Redisconf.Pass,
+		}),
+		KafkaPusher: kq.NewPusher(c.Kafka.Brokers, c.Kafka.Topic),
 	}, nil
 }
