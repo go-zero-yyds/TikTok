08bea3539b8680e24cde8858365cc7a83c4db7e6
lucky-zzz <1418170131@qq.com>
2023-08-28 12:41:51 +0800

    feat:

--- apps/video/rpc/internal/config/config.go
+++ apps/video/rpc/internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
@@ -10,6 +11,18 @@ type Config struct {
 	DBSource  string
 	Cache     cache.CacheConf
 	Snowflake SnowflakeConf
+
+	Redisconf    redis.RedisConf
+	HotVideoConf struct {
+		Windowsize   int
+		HotScore     int
+		ExpireMinute int
+		JobSpec      string
+	}
+	Kafka struct {
+		Brokers []string
+		Topic   string
+	}
 }
 
 type SnowflakeConf struct {
