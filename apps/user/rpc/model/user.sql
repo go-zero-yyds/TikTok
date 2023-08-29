108bb51bfa92dbfa2d0e189c1e33e1988ed87819
r27153733 <r27153733@gmail.com>
2023-08-29 22:50:44 +0800

    refactor: 规范sql文件。

--- apps/user/rpc/model/user.sql
+++ apps/user/rpc/model/user.sql
@@ -1,3 +1,10 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
                         `user_id` bigint(20) unsigned NOT NULL COMMENT '唯一用户ID，使用雪花算法生成',
                         `username` varchar(256) NOT NULL COMMENT '用户注册用户名，最长256个字符，唯一',
@@ -7,4 +14,6 @@ CREATE TABLE `user` (
                         `signature` varchar(32) NOT NULL COMMENT '个人简介',
                         PRIMARY KEY (`user_id`),
                         UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='TikTok用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_as_ci COMMENT='TikTok用户表';
+
+SET FOREIGN_KEY_CHECKS = 1;
--- apps/video/rpc/model/video.sql
+++ apps/video/rpc/model/video.sql
@@ -1,12 +1,18 @@
--- Active: 1682338800757@@127.0.0.1@3306@tiktok_video
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE
-    `video` (
-        `video_id` bigint(20) unsigned NOT NULL COMMENT '唯一视频ID，使用雪花算法生成',
-        `user_id` bigint(20) unsigned NOT NULL COMMENT '视频作者用户ID',
-        `play_url` varchar(256) NOT NULL COMMENT '视频播放地址, key',
-        `cover_url` varchar(256) NOT NULL COMMENT '视频封面地址, key',
-        `title` varchar(256) NOT NULL COMMENT '视频标题',
-        `create_time` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '创建时间',
-        PRIMARY KEY (`video_id`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'TikTok视频表';
+-- ----------------------------
+-- Table structure for video
+-- ----------------------------
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video` (
+                         `video_id` bigint(20) unsigned NOT NULL COMMENT '唯一视频ID，使用雪花算法生成',
+                         `user_id` bigint(20) unsigned NOT NULL COMMENT '视频作者用户ID',
+                         `play_url` varchar(256) NOT NULL COMMENT '视频播放地址, key',
+                         `cover_url` varchar(256) NOT NULL COMMENT '视频封面地址, key',
+                         `title` varchar(256) NOT NULL COMMENT '视频标题',
+                         `create_time` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '创建时间',
+                         PRIMARY KEY (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_as_ci COMMENT='TikTok视频表';
+
+SET FOREIGN_KEY_CHECKS = 1;
