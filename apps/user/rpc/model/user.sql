7e06289d4510e74657344aded0819b2da4791851
r27153733 <r27153733@gmail.com>
2023-08-30 20:17:25 +0800

    refactor: 规范sql文件。

--- apps/user/rpc/model/user.sql
+++ apps/user/rpc/model/user.sql
@@ -7,8 +7,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
                         `user_id` bigint(20) unsigned NOT NULL COMMENT '唯一用户ID，使用雪花算法生成',
-                        `username` varchar(256) NOT NULL COMMENT '用户注册用户名，最长256个字符，唯一',
-                        `password` varchar(256) NOT NULL COMMENT '密码，最长256字符',
+                        `username` varchar(32) NOT NULL COMMENT '用户注册用户名，最长32个字符，唯一',
+                        `password` varchar(256) NOT NULL COMMENT '密码，最长256字符，加密',
                         `avatar` varchar(256) NOT NULL COMMENT '用户头像',
                         `background_image` varchar(256) NOT NULL COMMENT '用户个人页顶部大图',
                         `signature` varchar(32) NOT NULL COMMENT '个人简介',
