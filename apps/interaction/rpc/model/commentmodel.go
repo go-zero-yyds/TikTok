44729b8113bd5ff7e20c231df3f2bcaec0e96656
lucky-zzz <1418170131@qq.com>
2023-09-02 01:05:39 +0800

    feat: list缓存

--- apps/interaction/rpc/model/commentmodel.go
+++ apps/interaction/rpc/model/commentmodel.go
@@ -11,7 +11,10 @@ import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
-var _ CommentModel = (*customCommentModel)(nil)
+var (
+	_                               CommentModel = (*customCommentModel)(nil)
+	cacheCommentCommentIdListPrefix              = "cache:comment:commentId:list"
+)
 
 type (
 	// CommentModel is an interface to be customized, add more methods here,
@@ -37,11 +40,17 @@ func NewCommentModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option)
 
 // CommentList 查看视频评论列表, 因为没分页，限制1000条。
 func (m *defaultCommentModel) CommentList(ctx context.Context, videoId int64) ([]*Comment, error) {
-	query := fmt.Sprintf("select * from %s where `video_id` = ? and is_deleted = '0' order by create_time desc limit 1000", m.table)
 	resp := make([]*Comment, 0)
+	key := fmt.Sprintf("%s%v", cacheCommentCommentIdListPrefix, videoId)
+	if err := m.CachedConn.GetCache(key, &resp); err == nil {
+		return resp, nil
+	}
+
+	query := fmt.Sprintf("select * from %s where `video_id` = ? and is_deleted = '0' order by create_time desc limit 1000", m.table)
 	err := m.QueryRowsNoCacheCtx(ctx, &resp, query, videoId)
 	switch {
 	case err == nil:
+		m.CachedConn.SetCacheCtx(ctx, key, &resp)
 		return resp, nil
 	case errors.Is(err, sqlc.ErrNotFound):
 		return nil, ErrNotFound
@@ -58,6 +67,7 @@ func (m *defaultCommentModel) TranInsert(ctx context.Context, s sqlx.Session, da
 		return nil, err
 	}
 	*keys = append(*keys, commentCommentIdKey)
+	m.OnChangeDeleteCache(ctx, data.VideoId)
 	return ret, err
 }
 
@@ -67,5 +77,13 @@ func (m *defaultCommentModel) TranUpdateDel(ctx context.Context, s sqlx.Session,
 	query := fmt.Sprintf("update %s set is_deleted = '1' where `comment_id` = ?", m.table)
 	_, err := s.ExecCtx(ctx, query, data.CommentId)
 	*keys = append(*keys, commentCommentIdKey)
+	m.OnChangeDeleteCache(ctx, data.VideoId)
 	return err
+}
+
+func (m *defaultCommentModel) OnChangeDeleteCache(ctx context.Context, videoId int64) {
+	deleteKeys := []string{
+		fmt.Sprintf("%s%v", cacheCommentCommentIdListPrefix, videoId),
+	}
+	m.CachedConn.DelCacheCtx(ctx, deleteKeys...)
 }
